cdc7c7500c97538f6c6b60ada146641b850b0f67
Gavin Belson <61915949+GavinBeIson@users.noreply.github.com>
2022-10-19 16:34:55 +0200

    Added polish, english localizations

--- src/main/java/com/example/bp/config/MvcConfig.java
+++ src/main/java/com/example/bp/config/MvcConfig.java
@@ -1,9 +1,18 @@
 package com.example.bp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -12,5 +21,24 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver slr = new CookieLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
     }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -67,84 +67,6 @@ document.addEventListener(
 			}
 		}
 
-		// Custom select
-		if (document.querySelector(".custom-select")) {
-			let x, i, j, l, ll, selElmnt, a, b, c;
-			x = document.getElementsByClassName("custom-select");
-			l = x.length;
-
-			for (i = 0; i < l; i++) {
-				selElmnt = x[i].getElementsByTagName("select")[0];
-				ll = selElmnt.length;
-				a = document.createElement("DIV");
-				a.setAttribute("class", "select-selected");
-				a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-				x[i].appendChild(a);
-				b = document.createElement("DIV");
-				b.setAttribute("class", "select-items select-hide");
-				for (j = 1; j < ll; j++) {
-					c = document.createElement("DIV");
-					c.innerHTML = selElmnt.options[j].innerHTML;
-					c.addEventListener("click", function () {
-						let y, i, k, s, h, sl, yl;
-						s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-						sl = s.length;
-						h = this.parentNode.previousSibling;
-						for (i = 0; i < sl; i++) {
-							if (s.options[i].innerHTML == this.innerHTML) {
-								s.selectedIndex = i;
-								h.innerHTML = this.innerHTML;
-								y = this.parentNode.getElementsByClassName("same-as-selected");
-								yl = y.length;
-								for (k = 0; k < yl; k++) {
-									y[k].removeAttribute("class");
-								}
-								this.setAttribute("class", "same-as-selected");
-								break;
-							}
-						}
-						h.click();
-					});
-					b.appendChild(c);
-				}
-				x[i].appendChild(b);
-				a.addEventListener("click", function (e) {
-					e.stopPropagation();
-					closeAllSelect(this);
-					this.nextSibling.classList.toggle("select-hide");
-					this.classList.toggle("select-arrow-active");
-				});
-			}
-
-			// Close all selects function
-			function closeAllSelect(e) {
-				let x,
-					y,
-					i,
-					xl,
-					yl,
-					arrNo = [];
-				x = document.getElementsByClassName("select-items");
-				y = document.getElementsByClassName("select-selected");
-				xl = x.length;
-				yl = y.length;
-				for (i = 0; i < yl; i++) {
-					if (e == y[i]) {
-						arrNo.push(i);
-					} else {
-						y[i].classList.remove("select-arrow-active");
-					}
-				}
-				for (i = 0; i < xl; i++) {
-					if (arrNo.indexOf(i)) {
-						x[i].classList.add("select-hide");
-					}
-				}
-			}
-
-			document.addEventListener("click", closeAllSelect);
-		}
-
 		// Search input
 		if (document.querySelector(".search-input")) {
 			let search = document.querySelector(".search-input input");
@@ -452,6 +374,101 @@ document.addEventListener(
 					// overlay.classList.remove("is-active");
 					videoContent.setAttribute("src", "");
 				});
+			}
+		}
+
+		//Lang Select
+		if (document.querySelector('.custom-select')) {
+
+			let x, i, j, l, ll, selElmnt, a, b, c;
+			x = document.getElementsByClassName("custom-select");
+			l = x.length;
+
+			for (i = 0; i < l; i++) {
+				selElmnt = x[i].getElementsByClassName("site-language")[0];
+				ll = selElmnt.length;
+				a = document.createElement("DIV");
+				a.setAttribute("class", "select-selected");
+				a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+				x[i].appendChild(a);
+				b = document.createElement("DIV");
+				b.setAttribute("class", "select-items select-hide");
+
+				for (j = 0; j < ll; j++) {
+					c = document.createElement("DIV");
+					c.innerHTML = selElmnt.options[j].innerHTML;
+					c.setAttribute("class", "select-item");
+					c.addEventListener("click", function (e) {
+						let y, i, k, s, h, sl, yl;
+						s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+						sl = s.length;
+						h = this.parentNode.previousSibling;
+						for (i = 0; i < sl; i++) {
+							if (s.options[i].innerHTML == this.innerHTML) {
+								s.selectedIndex = i;
+								h.innerHTML = this.innerHTML;
+								y = this.parentNode.getElementsByClassName("same-as-selected");
+								yl = y.length;
+								for (k = 0; k < yl; k++) {
+									y[k].classList.remove("same-as-selected");
+								}
+								this.classList.add("same-as-selected");
+								break;
+							}
+						}
+						h.click();
+						let selectedOption = h.textContent;
+						if (selectedOption != ''){
+							window.location.replace('?lang=' + selectedOption);
+						}
+						$('#locales').change();
+						$('#locales').val(selectedOption)
+
+						let langIcon = document.querySelector('.lang-icon img');
+						let chosenLang = this.innerHTML.toLowerCase();
+						langIcon.src = `./img/icons/flags/flag-${chosenLang}.svg`;
+					});
+					b.appendChild(c);
+				}
+
+				x[i].appendChild(b);
+				a.addEventListener("click", function (e) {
+					e.stopPropagation();
+					closeAllSelect(this);
+					this.nextSibling.classList.toggle("select-hide");
+					this.classList.toggle("select-arrow-active");
+					// console.log(a)
+				});
+			}
+
+			// Close all selects function
+			function closeAllSelect(e) {
+				let x, y, i, xl, yl, arrNo = [];
+				x = document.getElementsByClassName("select-items");
+				y = document.getElementsByClassName("select-selected");
+				xl = x.length;
+				yl = y.length;
+				for (i = 0; i < yl; i++) {
+					if (e == y[i]) {
+						arrNo.push(i)
+					} else {
+						y[i].classList.remove("select-arrow-active");
+					}
+				}
+				for (i = 0; i < xl; i++) {
+					if (arrNo.indexOf(i)) {
+						x[i].classList.add("select-hide");
+					}
+				}
+			}
+			let langItem = document.querySelectorAll(".select-item");
+
+			for (let i of langItem) {
+				let chosenLang = i.innerHTML.toLowerCase();
+				i.style.backgroundImage = `url('./img/icons/flags/flag-${chosenLang
+				}.svg')`;
+
+				document.addEventListener("click", closeAllSelect);
 			}
 		}
 	},
